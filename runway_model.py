c070c45680ceeb1256bc5ea90c80a07c3ec803ae
Ubuntu <ubuntu@ip-172-31-38-190.ec2.internal>
2019-10-30 09:12:04 +0000

    penultimate commit

--- runway_model.py
+++ runway_model.py
@@ -38,38 +38,30 @@ def compute_flow(network, inputs):
     current_frame = np.array(inputs["input_image"])
 
     if initialize:
-        print("Intializd")
-        a = current_frame
+        frame = current_frame
         f.append(current_frame)
         initialize = False
-        print(initialize)
+
     else:
-        print("You're good if this prints")
         f.append(current_frame)
-        print(f)
         prev_frame = f.pop(0)
-        print(prev_frame)
         next_frame = f[0]
 
         tensorFirst = torch.FloatTensor(np.array(prev_frame)[:, :, ::-1].transpose(2, 0, 1).astype(np.float32) * (1.0 / 255.0))
         tensorSecond = torch.FloatTensor(np.array(next_frame)[:, :, ::-1].transpose(2, 0, 1).astype(np.float32) * (1.0 / 255.0))
 
         tensorOutput = estimate(network, tensorFirst, tensorSecond)
-        print(tensorOutput)
-
-        #out = tempfile.NamedTemporaryFile(suffix=".flo")
-        #objectOutput = str(Path(out.name))
-        objectOutput = open("./lol.flo", 'wb')
+        
+        objectOutput = tempfile.NamedTemporaryFile(suffix=".flo")
+        out = str(Path(objectOutput.name))
         np.array([80, 73, 69, 72], np.uint8).tofile(objectOutput)
         np.array([tensorOutput.size(2), tensorOutput.size(1)], np.int32).tofile(objectOutput)
         np.array(tensorOutput.numpy().transpose(1, 2, 0), np.float32).tofile(objectOutput)
         
-        a = flowiz.convert_from_file(str(Path('lol.flo')))
-        print(a)
-        print(type(a))
-    
+        frame = flowiz.convert_from_file(out)
+        
 
-    return {"output_image": a}
+    return {"output_image": frame}
 
 
 if __name__ == "__main__":
